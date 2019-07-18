3125bc7731aef98344ea6b43cc3363535aa34eb1
SeiyaIwabuchi <0123ook@gmail.com>
2019-07-18 15:05:01 +0900

    問題データをHTMLに反映するメソッドを書いてる

--- server.py
+++ server.py
@@ -5,21 +5,67 @@ from flask import Flask, render_template
 from flask import request, jsonify
 import json
 
-app = Flask(__name__,template_folder="./")
+htmlSourcePath = "./index.html"
+probremsFilePath = "./probrems.json"
 
+app = Flask(__name__,template_folder="./")
+#セッション
+sessions = [0]
+
+#問題データセット
+probrems = object()
+
+#成績データ
+class RecordData():
+   totalAnswers = 0
+   correctAnswers = 0
+   wrongAnswers = 0
+   def getStatistics(self):
+      data = [ 
+         self.totalAnswers,
+         self.correctAnswers,
+         self.wrongAnswers,
+         self.correctAnswers / self.totalAnswers,
+         self.wrongAnswers / self.totalAnswers
+         ]
+      return data
+   correctNumber = []
+   wrongNumber = []
+
+#jqueryでif文で必ず回答しないと送信できないようにする。  
+
+#問題はjsonにする
+#問題データ読み込み
+def loadProbremsFromJson():
+   global probrems
+   with open(probremsFilePath,"r",encoding="utf-8_sig") as prob:
+      probrems = json.load(prob)
+
+#htmlに問題データを乗せる
+def problemWritingToHtml(probremNum):
+   pass
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   global sessions
+   with open(htmlSourcePath,'r',encoding="utf-8_sig") as htso:
+      htmlSource = htso.read()%sessions[len(sessions)-1]
+      sessions.append(sessions[len(sessions)-1] + 1)
+   return htmlSource
 
 @app.route('/postText', methods=['POST'])
 def receiveAnswer():
    radioRes = []
    for i in range(4):
       radioRes.append(request.json['radio%d'%(i+1)])
+   print("sessionID : " + request.json["sessionID"],end="")
    print(radioRes)
    #return jsonify(ResultSet=json.dumps(return_data))
 
+def raidoRes2Number(radioRes):
+   for index,b in enumerate(radioRes):
+      if b:
+         return index
 
 if __name__ == '__main__':
    app.run(host="127.0.0.1", port=8080)
--- static/js/sample.js
+++ static/js/sample.js
@@ -5,17 +5,36 @@ function init() {
   $("#Correct").hide();
   $("#next").hide();
   $("#ansButton").click(function() {
-    var textData = JSON.stringify({"radio1":$("input[name=rad]:checked").val() === "1","radio2":$("input[name=rad]:checked").val() === "2","radio3":$("input[name=rad]:checked").val() === "3","radio4":$("input[name=rad]:checked").val() === "4"});
-    $.ajax({
-      type:'POST',
-      url:'/postText',
-      data:textData,
-      contentType:'application/json',
-      success:function(data) {
-        var result = JSON.parse(data.ResultSet).result;
-        $("#hello").text(result);
-      }
-    });
+    var textData = JSON.stringify(
+      { "radio1":$("input[name=rad]:checked").val() === "1",
+        "radio2":$("input[name=rad]:checked").val() === "2",
+        "radio3":$("input[name=rad]:checked").val() === "3",
+        "radio4":$("input[name=rad]:checked").val() === "4",
+        "sessionID":$("#sessionID").text()
+      });
+    var answered = false;
+    if( $("input[name=rad]:checked").val() == "1" || 
+        $("input[name=rad]:checked").val() == "2" || 
+        $("input[name=rad]:checked").val() == "3" || 
+        $("input[name=rad]:checked").val() == "3" || 
+        $("input[name=rad]:checked").val() == "4")
+    {
+      answered = true;
+    }
+    if(answered){
+      $.ajax({
+        type:'POST',
+        url:'/postText',
+        data:textData,
+        contentType:'application/json',
+        success:function(data) {
+          var result = JSON.parse(data.ResultSet).result;
+          $("#hello").text(result);
+        }
+      });
+    }else{
+      alert("必ず回答してください。");
+    }
     return false;
   });
 }
